4a327956fe466edfbfac5d68f05bf34f2e0c860f
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2025-07-27 20:15:10 +0530

    Setup Spring Boot for CoDev Backend

---
+++ src/main/java/com/codev/backend/CoDevApplication.java
@@ -0,0 +1,13 @@
+package com.codev.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codev/backend/CoDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codev.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
