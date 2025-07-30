89ed4717cb2d909b9b3498a605665945de4eab1c
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2025-07-30 12:14:51 +0530

    Added CORS configuration for backend communication with frontend

---
+++ src/main/java/com/codev/backend/configuration/WebConfig.java
@@ -0,0 +1,26 @@
+package com.codev.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.micrometer.common.lang.NonNull;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**") // or "/api/**" for more restrictive matching
+                        .allowedOrigins("http://localhost:5173") // Vite dev server
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
