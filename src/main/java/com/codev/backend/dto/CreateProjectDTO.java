c5815825c77483bdd2085094367c178e99311378
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2025-08-02 00:50:54 +0530

    feat: add CreateProjectDTO for handling new project creation requests

---
+++ src/main/java/com/codev/backend/dto/CreateProjectDTO.java
@@ -0,0 +1,21 @@
+package com.codev.backend.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateProjectDTO {
+    private String title;
+    private String description;
+    private String domain;
+    private List<String> techStack;
+    private String status;
+    private String visibility;
+}
