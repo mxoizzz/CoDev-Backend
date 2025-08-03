603237dc8f38fcd04c5adf7d1f1d7a8d77ddd105
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2025-08-03 11:13:23 +0530

    feat: Create CollaborationRequest entity and status enum for handling project collaboration workflow

---
+++ src/main/java/com/codev/backend/entity/CollaborationRequest.java
@@ -0,0 +1,46 @@
+package com.codev.backend.entity;
+
+import java.time.LocalDateTime;
+
+import com.codev.backend.enums.CollaborationStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "collaboration_requests")
+public class CollaborationRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @Enumerated(EnumType.STRING)
+    private CollaborationStatus status;
+
+    private LocalDateTime sentAt;
+}
---
+++ src/main/java/com/codev/backend/enums/CollaborationStatus.java
@@ -0,0 +1,7 @@
+package com.codev.backend.enums;
+
+public enum CollaborationStatus {
+    Pending,
+    Accepted,
+    Rejected
+}
