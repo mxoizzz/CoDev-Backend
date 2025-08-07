e82435d8f7093510bb1a86784a4144d62d117b1d
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2025-08-07 23:12:35 +0530

    feat: add TeamDTO with required fields

---
+++ src/main/java/com/codev/backend/dto/TeamDTO.java
@@ -0,0 +1,22 @@
+package com.codev.backend.dto;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TeamDTO {
+    private Long id;
+    private String name;
+    private ProjectDTO project;
+    private Set<UserDTO> members;
+    private LocalDateTime createdAt;
+}
